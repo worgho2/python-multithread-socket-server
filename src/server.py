ef2cf6515828c5aa81318c1a1aa812fa981f32ad
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2022-04-01 14:23:54 -0300

    Added comments to server

--- src/server.py
+++ src/server.py
@@ -10,42 +10,57 @@ import sys
 
 class Server(object):
     def __init__(self):
+        # Socket server declaration
         self.tcp = socket(AF_INET, SOCK_STREAM)
         self.tcp.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+        # Variable that stores registered routes
         self.routes: List[Tuple[str, str, str]] = []
+
+        # Custom interruption handler
         signal(SIGINT, self.signalHandler)
 
     def listen(self, port: int, callback=None):
+        # Start socket server on given port
         self.tcp.bind(('0.0.0.0', port))
         self.tcp.listen(1)
+
+        # Execute custom anonymous function
         callback()
 
         while True:
+            # On new connections, create a new thread and call the handle function
             connection, client = self.tcp.accept()
             thread = Thread(target=self.handle, args=(connection, client))
             thread.start()
 
     def handle(self, connection: socket, client: Tuple[str, int]):
+        # Parse connection parameters
         method, path, httpVersion = connection.recv(1024).decode("utf-8").split('\r\n')[0].split(' ')
         address, port = client
 
+        # Print connection log
         print("%s (%s:%s) [%s] %s %s" %(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), address, port, method, path, httpVersion))
 
+        # Verify if the connection can be resolved with registered routes
         for route in self.routes:
-            if route[0] == path:               
+            if route[0] == path:    
+                # Return the content of registered route    
                 file = open(os.path.join(os.path.dirname(__file__), route[2]), 'rb')
                 connection.send(Server.encodedHttpResponse(200, route[1], file.read()))
-                
                 file.close()
                 connection.close()
                 return
 
+        # Return not found exception
         connection.send(Server.encodedHttpResponse(404, 'text/plain', f'No resource was found at \'{path}\''.encode()))
         connection.close()
 
+    # Register route
     def registerRoute(self, route: str, contentType: str, filePath: str):
         self.routes.append((route, contentType, filePath))
     
+    # Build encoded http response
     def encodedHttpResponse(status: int, contentType: str, body: bytes):
         httpVersion = 'HTTP/1.1'
         statusMessage = 'OK' if status == 200 else 'Not Found'
@@ -57,6 +72,7 @@ class Server(object):
         httpPesponse = f'{httpVersion} {status} {statusMessage}\r\nDate: {date}\r\nServer: {server}\r\nContent-Length: {contentLenght}\r\nConnection: {connection}\r\nContent-Type: {contentType}; charset={charset}\r\n\r\n'
         return httpPesponse.encode() + body
     
+    # Handle system SIGINT interruption
     def signalHandler(self, sig, frame):
         self.tcp.close()
         self.tcp.shutdown(sig)
